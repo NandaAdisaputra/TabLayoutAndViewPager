d7f81296bcc8c6239165f6f00e22cdd671128329
NandaAdisaputra <nandaadisaputra18@gmail.com>
2022-10-28 01:17:22 +0700

    tablayoutandviewpager

---
+++ app/src/main/java/com/nandaadisaputra/viewpagerandtablayout/activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.nandaadisaputra.viewpagerandtablayout.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaadisaputra.viewpagerandtablayout.databinding.ActivityMainBinding
+import com.nandaadisaputra.viewpagerandtablayout.fragment.FragmentOne
+import com.nandaadisaputra.viewpagerandtablayout.fragment.FragmentTwo
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        smartTabLayout()
+    }
+    private fun smartTabLayout() {
+
+        val adapter = FragmentPagerItemAdapter(supportFragmentManager, FragmentPagerItems.with(this)
+            .add("Fragment 1", FragmentOne::class.java)
+            .add("Fragment 2", FragmentTwo::class.java)
+            .create())
+
+        binding.viewPager.adapter = adapter
+        binding.viewPager.offscreenPageLimit = 0
+        binding.tabLayout.setViewPager(binding.viewPager)
+    }
+}
